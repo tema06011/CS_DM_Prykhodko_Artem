98ff7829ed0d4c4a43fb802f1a91af866d03fc88
tema06011 <64461451+tema06011@users.noreply.github.com>
2023-04-18 15:35:10 +0300

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -2,8 +2,8 @@ def read_graph(filename):
     with open(filename) as f:
         return [[int(num) for num in line.split()] for line in f]
 
-graph1 = read_graph('graph1.txt')
-graph2 = read_graph('graph2.txt')
+graph1 = read_graph('graph1_lab_5.txt')
+graph2 = read_graph('graph2_lab_5.txt')
 
 if graph1 != graph2 and list(map(len, graph1)) != list(map(len, graph2)):
     print("Not isomorphic")
